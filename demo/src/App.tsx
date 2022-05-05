2626f402dc838fb1ce1236c9a185858c9f356db4
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-05-05 14:31:58 +0400

    Start with demo' gp

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'mui-notify'
 import 'mui-notify/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Example 😄' />
 }
 
 export default App
