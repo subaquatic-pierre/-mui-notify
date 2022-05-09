1dbbbb1783dfadbb718476110b90a2b41bfa38aa
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-05-09 14:28:17 +0400

    Update settings info text

--- demo/src/components/Instructions.tsx
+++ demo/src/components/Instructions.tsx
@@ -14,16 +14,8 @@ const Instructions = () => {
       </Box>
       <Box>
         <Typography>
-          Choose wether the notification should be dismissible or not, by
-          default dismissible is set to false.
-        </Typography>
-      </Box>
-      <Box>
-        <Typography>
           Set the duration of the notification to be displayed. If the value is
-          set to 0 it will default to 5000 ms. If you wish to set the
-          notification to remain forever you will need to enable dismissible and
-          set duration to 0
+          set to 0 the notification will not timeout.
         </Typography>
       </Box>
       <Box>
